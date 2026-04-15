03e236a80bf8ff0651898bf168fa5e3bd9fe0cb6
rlatjsgh1029 <rlatjsgh1029@gmail.com>
2026-04-15 14:27:49 +0900

    variables declaration

--- Chapter1_1/mainchapter1.cpp
+++ Chapter1_1/mainchapter1.cpp
@@ -1,8 +1,10 @@
-#include<iostream>
+//프로그램의 구조
+
+#include<iostream> //입출력을 도와주는 기본 내장 기능
 
 int main(void)
 {
-	int x = 2; //literal
+	int x = 2; // 2,3,5같은 것들을 literal(리터럴)이라고 부른다.
 	x = 5;
 	int y = x + 3;
 
