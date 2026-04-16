8557412c24e10555755afd5c313199382c505fe4
rlatjsgh1029 <rlatjsgh1029@gmail.com>
2026-04-16 15:55:37 +0900

    Work Study

---
+++ Chapter1_6/Chapter1_6.cpp
@@ -0,0 +1,51 @@
+/*
+용어정리
+식별자:Identifier
+변수, 함수, 객체,...모든 이름이 Identifier 라고 부른다.
+무엇을 인식하느냐는 메모리 주소를 인식한다,
+그것을 프로그래머가 인식할 수 있는 이름으로 바꿔준다 라고 이해하면 된다.
+*/
+
+//함수 이름이 겹치게되면 Identifier를 할 수가 없고,
+//main 같은 경우는 오퍼레이팅 시스템이 가장 먼저 찾는 것이기에
+// 리졸브드 키워드인 것 처럼 작동을 한다.
+
+
+// 함수의 경우에는 보통은 대문자로 많이 시작한다.
+// 함수의 명이 길어질 경우에는 이름은 짧게 쓰고 주석을 이용해 기능에 관한 설명을 적어준다. 
+void addNumber()
+{
+
+
+}
+
+class MyClass
+{
+public:
+	int m_variable;
+};
+
+int main()
+{
+	/*	변수 명을 설정 할때 숫자로 시작할 수 없다.
+		띄어쓰기는 언더 바로 입력한다.*/
+	int n_apples;
+	int nApples;
+	int _apples;
+
+	addNumber()
+
+
+}
+
+//예제
+
+int total;//true
+int _orange;//false
+int _Value;//false
+int _my variable name; //false
+int TitalCustomers;
+int void; //false
+int numFruit;//true
+int 2some;//flase
+int meters_of_pipe;//ture
